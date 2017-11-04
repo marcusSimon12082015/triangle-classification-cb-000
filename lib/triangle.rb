1fd0bd7fabb3bab173cbd699d5740a0a302c5d15
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-04 18:53:03 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,14 @@
 class Triangle
   # write code here
-  attr_accessor :equilateral, :isosceles, :scalene
-  def kind
-    if 
+  attr_accessor 
+  def kind(side1,side2,side3)
+    
+  end
+  def valid?(side1,side2,side3)
+    side1, side2, side3 > 0
   end
 end
 
 class TriangleError < StandardError
-  
+
 end
