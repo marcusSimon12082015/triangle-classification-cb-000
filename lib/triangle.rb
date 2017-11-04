5e486531686d89ac976b362c0be9c9b7a5036583
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-04 19:06:59 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,10 +3,13 @@ class Triangle
   attr_accessor
   def kind(side1,side2,side3)
     if valid?
-
+      
     else
 
     end
+  end
+  def equilateral?(side1,side2,side3)
+    side1 == side2 == side3
   end
   def valid?(side1,side2,side3)
     (side1 && side2 && side3) > 0 && triangle_inequality?
