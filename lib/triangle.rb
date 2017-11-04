f5f85ba8624985bd1dbc0d9612dac867a11afaf1
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-04 19:04:05 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,9 @@ class Triangle
   def valid?(side1,side2,side3)
     (side1 && side2 && side3) > 0 && (side1 + side2 > side3)
   end
-  
+  def triangle_inequality(side1,side2,side3)
+    (side1 + side2 > side3) && (side2 + side3 > side1) && (side3 + side1 > side2)
+  end
 end
 
 class TriangleError < StandardError
