c71ac0a14cba384372c3dce4cbd9e4f7d6ea6539
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-04 18:53:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,11 @@ class Triangle
   # write code here
   attr_accessor
   def kind(side1,side2,side3)
-    
+    if valid?
+      
+    else
+
+    end
   end
   def valid?(side1,side2,side3)
     side1, side2, side3 > 0
