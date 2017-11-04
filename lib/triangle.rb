44a400d93e3c3fc50460356bfa98ee61aa6383b8
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-04 19:27:27 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,43 @@
 class Triangle
   # write code here
+  attr_accessor :side1,:side2,:side3
+  def initialize(side1,side2,side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  def kind
+    if valid?
+      if equilateral?
+        :equilateral
+      elsif isosceles?
+        :isosceles
+      elsif scalene?
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+  def equilateral?
+    @side1 == @side2 && @side1 == @side3
+  end
+  def isosceles?
+    @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+  end
+  def scalene?
+    @side1 != @side2 && @side2 != @side3 && @side1 != @side3
+  end
+  def valid?
+    (@side1 && @side2 && @side3) > 0 && triangle_inequality?
+  end
+  def triangle_inequality?
+    (@side1 + @side2 > @side3) && (@side2 + @side3 > @side1) && (@side3 + @side1 > @side2)
+  end
+end
+
+class TriangleError < StandardError
+  def message
+    "Error Triangle not valid!"
+  end
 end
